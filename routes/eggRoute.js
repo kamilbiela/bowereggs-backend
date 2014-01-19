421c3a02dbb7d523841dc1efdbc67e2a858f16fb
Kamil Biela <kamil.biela@gmail.com>
2014-01-20 00:38:55 +0100

    search methods improvements

--- routes/eggRoute.js
+++ routes/eggRoute.js
@@ -1,4 +1,6 @@
 var _ = require('lodash');
+var when = require('when');
+var parallel = require('when/parallel');
 
 module.exports = function(server, Egg) {
     server.get('egg/:name', function(req, res, next) {
@@ -22,54 +24,100 @@ module.exports = function(server, Egg) {
         var page = parseInt(req.query.page) || 1;
         var limit = 50;
 
-        var cleanText = function(text) {
+        function cleanText(text) {
             text = text.replace(/[^\w\ \-\*]/i, '');
             text = text.replace(/\*/g, '.*');
             return text;
         };
 
-        var buildOrCondition = function(text, byName, byDescription, byKeywords) {
+        function buildOrCondition(text, byName, byDescription, byKeywords) {
             var or = [];
-            if (byName) {
+            if (byName && byName === 'true') {
                 or.push({name: new RegExp('^' + text + '$')});
             }
 
-            if (byDescription) {
+            if (byDescription && byDescription === 'true') {
                 or.push({description: new RegExp(text)});
             }
 
-            if (byKeywords) {
+            if (byKeywords && byKeywords === 'true') {
                 or.push({keywords: text});
             }
 
             return or;
         }
 
-        var conditions = {
-            existsOnGithub: true
-        };
+        function buildSearchConditions() {
+            var conditions = {
+                existsOnGithub: true
+            };
 
-        if (req.query.search) {
-            var text = cleanText(req.query.search);
+            if (req.query.search) {
+                var text = cleanText(req.query.search);
 
-            var or = buildOrCondition(text, req.query.byName, req.query.byDescription, req.query.byKeywords);
-            if (or.length > 1) {
-                conditions["$or"] = or;
-            } else if (or.length === 1) {
-                _.assign(conditions, or[0]);
+                var or = buildOrCondition(text, req.query.byName, req.query.byDescription, req.query.byKeywords);
+                if (or.length > 1) {
+                    conditions["$or"] = or;
+                } else if (or.length === 1) {
+                    _.assign(conditions, or[0]);
+                }
             }
+
+            return conditions;   
         }
 
-        console.log(conditions)
+        function getResponseSkeleton() {
+            return {
+                meta: {
+                    count: 0,
+                    page: page,
+                    totalPages: 0
+                },
+                data: null
+            };
+        }
 
-        Egg.find(conditions).skip(limit * (page - 1)).limit(limit).exec(function(err, eggs) {
-            if (err) {
-                return next(err);
+        function buildFindPromise() {
+            return when.promise(function(resolve, reject) {
+                Egg.find(conditions).skip(limit * (page - 1)).sort({stargazersCount: -1}).limit(limit).exec(function(err, eggs) {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    response.data = eggs;
+                    return resolve();
+                });
+            });            
+        }
+
+        function buildCountPromise() {
+            return when.promise(function(resolve, reject) {
+                Egg.count(conditions).exec(function(err, count) {
+                    if (err) {
+                        return reject(err)
+                    }
+
+                    response.meta.count = count;
+                    response.meta.totalPages = Math.ceil(count / limit);
+                    return resolve();
+                });
+            });
+        }
+
+        var conditions = buildSearchConditions();
+        var response = getResponseSkeleton();
+        
+        parallel([
+            function() {
+                return buildFindPromise();
+            }, function() {
+                return buildCountPromise();
             }
-
-            res.json(eggs);
-            return next();
+        ]).done(function() {
+            res.json(response);
+            next(); 
+        }, function(err) {
+            next(err);
         });
-
     });
 }
